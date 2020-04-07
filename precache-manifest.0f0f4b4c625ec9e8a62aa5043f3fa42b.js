b07e619bdccbab25ca6670f310f29b0267fe3a70
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-04-07 22:57:15 +0700

    Updates

---
+++ precache-manifest.0f0f4b4c625ec9e8a62aa5043f3fa42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afc3c313128257b245bf23561a0e781",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "9761b3479b4ca7f36603",
+    "url": "/react-pagination/static/css/2.03ab5855.chunk.css"
+  },
+  {
+    "revision": "1f21da1ad499bf2b26ff",
+    "url": "/react-pagination/static/css/main.98a34aa9.chunk.css"
+  },
+  {
+    "revision": "9761b3479b4ca7f36603",
+    "url": "/react-pagination/static/js/2.21993088.chunk.js"
+  },
+  {
+    "revision": "1f21da1ad499bf2b26ff",
+    "url": "/react-pagination/static/js/main.9acc1359.chunk.js"
+  },
+  {
+    "revision": "8448d5fefe399b1028a5",
+    "url": "/react-pagination/static/js/runtime-main.8464c604.js"
+  }
+]);
